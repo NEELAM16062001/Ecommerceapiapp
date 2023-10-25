475baae6f2a1547abc8371ed74923d14c41673e0
unknown <harishneela71@gmail.com>
2023-10-25 15:41:02 +0530

    updated

---
+++ config/mongoose.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+// connecting mongoose to its default server and ecommerceDB
+mongoose.connect('mongodb+srv://harishneelam:jQ0RWw6yJLbmUsuo@cluster0.sjevfuc.mongodb.net/?retryWrites=true&w=majority');
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
+
+db.once('open', function(){
+    console.log('Connected to Database :: MongoDB');
+});
+
+module.exports = db;
